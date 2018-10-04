999d5a71f810cf1a105c69dbbc62fb0edbdd48e9
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-03 18:28:11 -0700

    Add project files.

---
+++ MiniaturesRUs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiniaturesRUs.Startup))]
+namespace MiniaturesRUs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
