3c1311efe75508b4a9f77af5bb50711a9b68b078
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-06 14:54:38 -0700

    set up database, added class enitity for Miniatures, and Added and Updated with a migration

--- MiniaturesRUs/Models/Miniature.cs
+++ MiniaturesRUs/Models/Miniature.cs
@@ -8,41 +8,41 @@ namespace MiniaturesRUs.Models
 {
     public class Miniature
     {
-        int MiniId { get; set; }
+        [Key]
+        public int Miniid { get; set; }
 
         [Required]
-        string Name { get; set; }
+        public string Name { get; set; }
 
         [Required]
-        double Price { get; set; }
+        public double Price { get; set; }
 
         [Required]
-        string Description { get; set; }
+        public string Description { get; set; }
 
         [Required]
-        DateTime Year { get; set; }
+        public DateTime Year { get; set; }
 
         [Required]
-        string GameName { get; set; }
+        public string GameName { get; set; }
 
         [Required]
-        string Faction { get; set; }
+        public string Faction { get; set; }
 
-        char Size { get; set; }
+        public char Size { get; set; }
 
-        int Speed { get; set; }
+        public int Speed { get; set; }
 
-        int Attack { get; set; }
+        public int Attack { get; set; }
 
-        int Strength { get; set; }
+        public int Strength { get; set; }
 
-        int HitPoints { get; set; }
+        public int HitPoints { get; set; }
 
-        int Defense { get; set; }
+        public int Defense { get; set; }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, string faction)
+        public Miniature(string name, double price, string description, DateTime year, string gameName, string faction)
         {
-            MiniId = miniId;
             Name = name;
             Price = price;
             Description = description;
@@ -51,44 +51,44 @@ namespace MiniaturesRUs.Models
             Faction = faction;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature( string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size) 
-            : this(miniId, name, price, description, year, gameName, faction)
+            : this(name, price, description, year, gameName, faction)
         {
             Size = size;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature(string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size, int speed) 
-            : this(miniId, name, price, description, year, gameName, faction, size)
+            : this( name, price, description, year, gameName, faction, size)
         {
             Speed = speed;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature(string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size, int speed, int attack) 
-            : this(miniId, name, price, description, year, gameName, faction, size, speed)
+            : this(name, price, description, year, gameName, faction, size, speed)
         {
             Attack = attack;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature(string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size, int speed, int attack, int strength) 
-            : this(miniId, name, price, description, year, gameName, faction, size, speed, attack)
+            : this(name, price, description, year, gameName, faction, size, speed, attack)
         {
             Strength = strength;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature(string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size, int speed, int attack, int strength, int hitPoints) 
-            : this(miniId, name, price, description, year, gameName, faction, size, speed, attack, strength)
+            : this(name, price, description, year, gameName, faction, size, speed, attack, strength)
         {
             HitPoints = hitPoints;
         }
 
-        public Miniature(int miniId, string name, double price, string description, DateTime year, string gameName, 
+        public Miniature(string name, double price, string description, DateTime year, string gameName, 
                         string faction, char size, int speed, int attack, int strength, int hitPoints, int defense) 
-            : this(miniId, name, price, description, year, gameName, faction, size, speed, attack, strength, hitPoints)
+            : this(name, price, description, year, gameName, faction, size, speed, attack, strength, hitPoints)
         {
             Defense = defense;
         }
