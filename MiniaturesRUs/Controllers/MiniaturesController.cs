3900a64c022a527a126453a98c60b7831c6c11fa
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-06 15:35:37 -0700

    Revert "Revert "Revert "Revert "Revert "Revert "Moved MiniatureController to controller folder, edited MiniatureControllers namespace to MiniaturesRUs.Controllers, and updated the _layout file to display a link to the Miniatures index""""""
    This reverts commit 0658ba7bd10b5f642bce4796a0855285fa2e4275.

--- MiniaturesRUs/Controllers/MiniaturesController.cs
+++ MiniaturesRUs/Controllers/MiniaturesController.cs
@@ -8,7 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using MiniaturesRUs.Models;
 
-namespace MiniaturesRUs.Views
+namespace MiniaturesRUs.Controllers
 {
     public class MiniaturesController : Controller
     {
