6e5d8570127ee680cbf332439554e0ab8fb87919
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-06 18:20:52 -0700

    Made DBHelper for Miniatures, and made first helper method GetMiniatureById()

---
+++ MiniaturesRUs/Models/MiniatureDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniaturesRUs.Models
+{
+    public static class MiniatureDB
+    {
+        public static Miniature GetMiniatureById(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            return db.Minitures.Find(id);
+        }
+
+
+
+    }
+}
