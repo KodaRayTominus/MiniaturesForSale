1a09b6eea6b90afe9a1d2b55f1ffe1c0e8ddb44c
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-06 20:32:34 -0700

    Set up first Unit test, Test checks default values where applicable. Needs Issue #38 finished to work

--- MiniaturesRUs.Tests/Models/MiniatureTests.cs
+++ MiniaturesRUs.Tests/Models/MiniatureTests.cs
@@ -11,17 +11,48 @@ namespace MiniaturesRUs.Models.Tests
     [TestClass()]
     public class MiniatureTests
     {
+        /*string name, double price, string description, DateTime year, string gameName, string faction, char? size, int? speed, 
+                        int? attack, int? strength, int? hitPoints, int? defense*/
+
         [TestMethod()]
-        public void MiniatureTest()
+        public void Miniature_CreateMiniature_CreatesMiniatureWithDefaultValuesWhereApplicable()
         {
 
             //Arrange 
+            string name = "testMini";
+
+            double price = 10.00;
+
+            string description = "This is a description";
+
+            DateTime year = 1992;
+
+            string gameName = "testGame";
+
+            string faction = "testFaction";
+
+            char? defaultSize = 'x';
+
+            int? defaultSpeed = 0;
+
+            int? defaultAttack = 0;
+
+            int? defaultStrength = 0;
+
+            int? DefaultHitPoints = 0;
+
+            int? defaultDefense = 0;
 
             //Act
+            Miniature miniToTest = new Miniature(name, price, description, year, gameName, faction);
 
             //Assert
-
-            Assert.Fail();
+            Assert.AreEqual(defaultSize, miniToTest.Size);
+            Assert.AreEqual(defaultSpeed, miniToTest.Speed);
+            Assert.AreEqual(defaultAttack, miniToTest.Attack);
+            Assert.AreEqual(defaultStrength, miniToTest.Strength);
+            Assert.AreEqual(DefaultHitPoints, miniToTest.HitPoints);
+            Assert.AreEqual(defaultDefense, miniToTest.Defense);
         }
     }
 }
