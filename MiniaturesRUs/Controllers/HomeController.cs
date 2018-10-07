a22795bececa10c43070f832def988691397735f
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-06 18:01:18 -0700

    Updated the contact page, added 2 more emails, and changed the address and phone number

--- MiniaturesRUs/Controllers/HomeController.cs
+++ MiniaturesRUs/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace MiniaturesRUs.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "We would love to hear from you!";
 
             return View();
         }
