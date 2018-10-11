9cc3c3cda3bbcd69548f7bece56e9abc20f70e27
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-10 22:42:41 -0700

    Added Fields to Person Class, Personid, name, address and UserName. Also added AccountId to link to account table at later time.

--- MiniaturesRUs/Models/Person.cs
+++ MiniaturesRUs/Models/Person.cs
@@ -7,7 +7,15 @@ namespace MiniaturesRUs.Models
 {
     public class Person
     {
+        public int Personid { get; set; }
 
+        private string Name { get; set; }
+
+        private string Address { get; set; }
+
+        private string UserName { get; set; }
+
+        private int AccountId { get; set; }
 
         public Person()
         {
