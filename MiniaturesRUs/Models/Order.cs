a5f214e95962f917a8770ef8715772d52e7e7288
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-11 12:37:55 -0700

    Added Fields to Order Class

--- MiniaturesRUs/Models/Order.cs
+++ MiniaturesRUs/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,24 @@ namespace MiniaturesRUs.Models
 {
     public class Order
     {
+        public int OrderId { get; set; }
+
+        [ForeignKey("People")]
+        [Column(Order = 1)]
+        public int BuyerId { get; set; }
+
+        [ForeignKey("People")]
+        [Column(Order = 2)]
+        public int SellerId { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(int buyerId, int sellerId)
+        {
+            BuyerId = buyerId;
+            SellerId = sellerId;
+        }
     }
 }
