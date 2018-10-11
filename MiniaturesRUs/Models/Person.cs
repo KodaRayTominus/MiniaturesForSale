bd7ec1ee31079a7b5d0af62f5c20fa6e6e95e06f
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-10 22:44:27 -0700

    Generated constructors

--- MiniaturesRUs/Models/Person.cs
+++ MiniaturesRUs/Models/Person.cs
@@ -20,5 +20,40 @@ namespace MiniaturesRUs.Models
         public Person()
         {
         }
+
+        public Person(int personid)
+        {
+            Personid = personid;
+        }
+
+        public Person(int personid, string name)
+        {
+            Personid = personid;
+            Name = name;
+        }
+
+        public Person(int personid, string name, string address)
+        {
+            Personid = personid;
+            Name = name;
+            Address = address;
+        }
+
+        public Person(int personid, string name, string address, string userName)
+        {
+            Personid = personid;
+            Name = name;
+            Address = address;
+            UserName = userName;
+        }
+
+        public Person(int personid, string name, string address, string userName, int accountId)
+        {
+            Personid = personid;
+            Name = name;
+            Address = address;
+            UserName = userName;
+            AccountId = accountId;
+        }
     }
 }
