a05c72a6a8672c74502e3cda0ff72bb5f5b91b79
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-11 12:41:42 -0700

    Added ProductOrder class and added fields and constructors

--- MiniaturesRUs/Models/ProductOrders.cs
+++ MiniaturesRUs/Models/ProductOrders.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace MiniaturesRUs.Models
 {
-    public class ProductOrders
+    public class ProductOrder
     {
+        [Key]
+        [Column(Order = 1)]
+        public int OrderId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int ProductId { get; set; }
+
+        public ProductOrder()
+        {
+        }
+
+        public ProductOrder(int orderId, int productId)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+        }
     }
 }
