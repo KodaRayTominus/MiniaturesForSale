89244765f195c5a47cba464ad826ee8d09825f90
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-11 00:02:59 -0700

    Added CRUD Methods into PersonDB

--- MiniaturesRUs/Models/PersonDB.cs
+++ MiniaturesRUs/Models/PersonDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,35 @@ namespace MiniaturesRUs.Models
 {
     public class PersonDB
     {
+        public static Person GetPersonById(int? id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            return db.People.Find(id);
+        }
+
+        public static void AddPersonToDB(Person person)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            db.People.Add(person);
+            db.SaveChanges();
+        }
+
+        public static void UpdatePerson(Person person)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            db.Entry(person).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public static void DeletePerson(Person person)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            db.People.Remove(person);
+            db.SaveChanges();
+        }
     }
 }
