88d4c63fd3c4315a17575cde05fc44724705b6db
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-13 20:13:24 -0700

    set up Order Unit test file

---
+++ MiniaturesRUs.Tests/Models/OrderTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniaturesRUs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniaturesRUs.Models.Tests
+{
+    [TestClass()]
+    public class OrderTests
+    {
+        [TestMethod()]
+        public void OrderTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
