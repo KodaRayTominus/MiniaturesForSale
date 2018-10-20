16a324da2b5990f4a10a57f8e1b6cf9050cb3a60
KodaRayTominus <36204580+KodaRayTominus@users.noreply.github.com>
2018-10-20 02:57:06 -0700

    Created MessageDB Helper Class, and implimented a GetAllMessagesForUserById Method.

---
+++ MiniaturesRUs/Models/MessageDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniaturesRUs.Models
+{
+    public static class MessageDB
+    {
+        public static List<PersonalMessage> GetAllMessageForUserById(string id)
+        {
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            List<PersonalMessage> messages = (from m in db.PersonalMessages
+                                              where m.RecipientID == id || m.SenderID == id
+                                              select m).ToList();
+
+            return messages;
+        }
+    }
+}
